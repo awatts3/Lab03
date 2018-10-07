4bed016cd3fe743dc738abd52ffc92ff6f2883a2
awatts3 <43153899+awatts3@users.noreply.github.com>
2018-10-07 11:21:02 -0600

    Update Lab03-awatts3.cpp

--- Lab03-awatts3.cpp
+++ Lab03-awatts3.cpp
@@ -8,7 +8,7 @@ using std::cin;
 using std::endl;
 
 
-short int n(0), sum(0);
+double n(0), sum(0);
 double product(1);
 
 
@@ -19,12 +19,10 @@ int main()
 	cout << "Input an 'n' to sum to: (0 or negative number to end)" << endl;
 	cin >> n;
 	if (n <= 0) { break; }
-
 	for (int i = 1; i <= n; i++)
 	{
-		sum = sum + i;
+	sum = sum + i;
 	}
-
 	cout << "The sum from 1 to " << n << " is " << sum << endl;
 	sum = 0; //restes the sum for the loop
 	}  //sum part, steps 1 - 3*/
@@ -32,36 +30,42 @@ int main()
 	//factorial part steps 4 - 5
 	/*while (n >= 0)
 	{
-		cout << "Input an 'n' compute factorial of: (0 or negative number to end)" << endl;
-		cin >> n;
-		if (n <= 0) { break; }
-
-		for (long int i = n; i >= 1; i--)
-		{
-			product = product * i;
-		}
-		cout << n << "!=" << product << endl;
-		product = 1; //resets the product for the loop
+	cout << "Input an 'n' compute factorial of: (0 or negative number to end)" << endl;
+	cin >> n;
+	if (n <= 0) { break; }
+	for (long int i = n; i >= 1; i--)
+	{
+	product = product * i;
+	}
+	cout << n << "!=" << product << endl;
+	product = 1; //resets the product for the loop
 	}*/
 
 	//step 6 
-	while (n >= 0)
+	/*while (n >= 0)
 	{
-		float nverse(1); //inverse of n, get it?
-		float total(0); //since sum is already the name of something
+	double nverse(1); //inverse of n, get it?
+		double total(0); //since sum is already the name of something
 		cout << "Input an 'n' sum the 1/n values of: (0 or negative number to end)" << endl;
 		cin >> n;
 		if (n <= 0) { break; }
-		
-		for (int i=1;i<=n;i++) 
+		nverse = 1 / n;
+	
+		for (int i = 1; i <= n; i++)
 		{
-			nverse = 1 / n;
 			total = total + nverse;
 		}
-		total = total - 1.0;
-		cout << "The sum for " << n << " values:" << total << endl;
+		total = total - 1;
+
+		cout << "The sum - 1 for " << n << " values:" << total << endl;
 		total = 0;
-	}
+	}*/
+
+	//step 7 and 8
+
+	for (double i = 3.4; i < 4.4; i += 0.2) 
+	{ cout << "i = " << i << endl; }
+
 	system("pause");
 	return(0);
 }
